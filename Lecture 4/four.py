6a7a7f41122f5d4e659d4b952677792af65957d5
Khushi Roy <khushinroy@gmail.com>
2025-08-06 17:44:21 +0530

    Add files via upload

---
+++ Lecture 4/four.py
@@ -0,0 +1,21 @@
+#Dictionary is mutable
+dict={
+    "key":"value",              # key can be anything which is immutable
+    "name":"Khushi",
+    "learning":["coding","maths","physics","chemistry","biology"],
+    "topics":("dict","set "),
+    "age": 25,
+    "marks":94.54,
+}
+print(dict)
+print(dict["name"])            # use square brackets for finding anything 
+dict["age"] =52,
+dict["surname"] = "Roy",
+print(dict) 
+null_dict={}
+print(null_dict)
+print("Hey world my life is busy as hell and peopele don'")
+
+
+
+print(dict["key"])
